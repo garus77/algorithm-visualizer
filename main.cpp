83df2548bbc5e8223d81e8fa5ac6bb966b9d07d5
garus77 <csatabedegaspar@gmail.com>
2025-06-14 17:50:41 +0200

    basic menu in cmd

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+enum Choices{
+    NONE=0,
+    BUBBLE_SORT, SELECTION_SORT, INSERTION_SORT,
+    MAX
+};
+
+constexpr const char* choiceNames[MAX]={
+    "None", "Bubble sort", "Selection sort", "Insertion sort"
+};
+
+constexpr const char* unknownValueError="Unknown value!\n";
 
 const sf::VideoMode windowBase(800,800);
 
@@ -24,12 +37,35 @@ void render(sf::RenderWindow& window){
     window.display();
 }
 
+void getChoice(int& choice){
+    while(true){
+        std::cout<<"******************************\nAlgorithms:\n";
+        for(int i=NONE+1; i<MAX; i++){
+            std::cout<<i<<" - "<<choiceNames[i]<<'\n';
+        }
+        std::cout<<"******************************\nGive a value for an algorithm to be visualized:\n";
+        if(!(std::cin>>choice)){
+            std::cin.clear();
+            std::cin.ignore(INT_MAX-1,'\n');
+            std::cout<<unknownValueError<<std::endl;
+            continue;
+        }
+        if(choice>NONE && choice<MAX){
+            std::cout<<"Visualizing ["<<choiceNames[choice]<<"]..."<<std::endl;
+            break;
+        }
+        std::cout<<unknownValueError<<std::endl;
+    }
+}
+
 int main(){
+    int choice=NONE;
+    getChoice(choice);
+
     bool running=true;
     sf::Clock clock;
     sf::RenderWindow window(windowBase,"TITLE",sf::Style::Close);
     window.setFramerateLimit(60);
-
     //game loop
     while(running){
         float deltaTime=clock.restart().asSeconds();
